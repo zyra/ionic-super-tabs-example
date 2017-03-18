a859b1b9d78fa8668ad731edb66d0eb0add53df4
Ibby <ibby93@gmail.com>
2017-03-17 22:28:02 -0400

    modularize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,24 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import {SuperTabsComponent} from "../components/super-tabs/super-tabs";
 import {Page1Page} from "../pages/page1/page1";
 import {Page2Page} from "../pages/page2/page2";
-import {SuperTabComponent} from "../components/super-tab/super-tab";
 import {Page3Page} from "../pages/page3/page3";
+
+import { SuperTabsModule } from 'ionic2-super-tabs';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    SuperTabsComponent,
-    SuperTabComponent,
     Page1Page,
     Page2Page,
     Page3Page
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    SuperTabsModule
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
