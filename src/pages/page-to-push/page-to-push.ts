def0b4fe45fa48cbdb7c33edd168502443ad3a85
Daniel <mytechde@outlook.com>
2017-05-14 16:45:52 +0200

    chore(version): updated to Ionic 3.2.1

--- src/pages/page-to-push/page-to-push.ts
+++ src/pages/page-to-push/page-to-push.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/*
-  Generated class for the PageToPush page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-page-to-push',
   templateUrl: 'page-to-push.html'
 })
 export class PageToPushPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams) { }
 
 }
