ec2569204ea192e2c070a3170a4d34a454f0afd7
Ibby Hadeed <ibby@zyra.ca>
2019-03-24 01:56:56 -0400

    Remove old code

--- src/pages/ionic-tabs/ionic-tabs.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-@IonicPage()
-@Component({
-  selector: 'page-ionic-tabs',
-  templateUrl: 'ionic-tabs.html',
-})
-export class IonicTabsPage {}
