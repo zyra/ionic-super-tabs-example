efdce47339c3196b442d1513d83eda25bff1ab13
Ibby Hadeed <ibby@zyra.ca>
2019-04-14 20:09:32 -0400

    Add ViewChild example

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { SuperTabs } from '@ionic-super-tabs/angular';
 import { ContactsPage } from '../contacts/contacts.page';
 import { ProfilePage } from '../profile/profile.page';
 
@@ -7,7 +8,15 @@ import { ProfilePage } from '../profile/profile.page';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
+
+  @ViewChild(SuperTabs) superTabs: SuperTabs;
+
   contactsPage = ContactsPage;
   profilePage = ProfilePage;
+
+  ngAfterViewInit() {
+    console.log('Super tabs is ', this.superTabs);
+    this.superTabs.selectTab(1);
+  }
 }
